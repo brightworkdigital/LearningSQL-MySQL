ad3d56edb28c5e1d4dba1c592a39eabc22e77214
BackpackTrainer <bill@fairfieldco.com>
2024-01-02 13:10:00 -0500

    Initial commit

---
+++ src/test/java/com/example/LearningSQL/LearningSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

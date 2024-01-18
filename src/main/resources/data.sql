74154d5c73a169d30891ebda0d7c7682ae753251
Dave <davedykes@davedykes.com>
2024-01-18 19:55:17 +0000

    Initial commit.  Working with MySQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,6 @@ VALUES
 
 INSERT INTO EMPLOYEES (first_name, last_name, email, company_id)
 VALUES
-    ('Dan', 'Vega', 'danvega@gmail.com', SELECT company_id FROM COMPANY WHERE company_name = 'IBM'),
-    ('Jen', 'Vega', 'jenvega@email.com', SELECT company_id FROM COMPANY WHERE company_name = 'Barclays'),
-    ('Bella', 'Vega', 'bellavega@gmail.com', SELECT company_id FROM COMPANY WHERE company_name = 'Fairfield & Company');
+    ('Dan', 'Vega', 'danvega@gmail.com', (SELECT company_id FROM COMPANY WHERE company_name = 'IBM')),
+    ('Jen', 'Vega', 'jenvega@email.com', (SELECT company_id FROM COMPANY WHERE company_name = 'Barclays')),
+    ('Bella', 'Vega', 'bellavega@gmail.com', (SELECT company_id FROM COMPANY WHERE company_name = 'Fairfield & Company'));
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS EMPLOYEES;
 DROP TABLE IF EXISTS COMPANY;
+
 CREATE TABLE COMPANY (
     company_id INT AUTO_INCREMENT  PRIMARY KEY,
     company_name VARCHAR(250) NOT NULL
@@ -11,6 +13,5 @@ CREATE TABLE EMPLOYEES (
     last_name VARCHAR(250) NOT NULL,
     email VARCHAR(250) DEFAULT NULL,
     company_id INT,
-    foreign key (company_id) references COMPANY
-);
+    foreign key (company_id) references COMPANY (company_id) );
 
